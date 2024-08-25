07b3681ce40ec3b4fdb85dbe0a188eb5485ef3c0
Tereza Mündleinová <tereza.mundleinova@gmail.com>
2024-08-25 10:37:32 +0200

    App amended - router adjusted Header and footer added

---
+++ src/components/organisms/KiosekFooter.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import KiosekContainer from '@/components/other/KiosekContainer.vue'
+</script>
+
+<template>
+  <footer>
+    <kiosek-container
+      :background-class="'bg-blue-dark'">
+      <div class="px-6 py-3">
+        <p class="text-xl text-white font-bold">
+          V případě poruchy kiosku, prosím, volejte: <strong>+420 796 123 456</strong>.
+        </p>
+      </div>
+    </kiosek-container>
+  </footer>
+</template>
---
+++ src/components/organisms/KiosekHeader.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import KiosekContainer from '@/components/other/KiosekContainer.vue'
+</script>
+
+<template>
+  <header>
+    <kiosek-container>
+      <div class="bg-blue-dark pl-6 pr-4 py-3 w-56 relative z-20 after:content-empty after:absolute after:block after:w-12 after:h-full after:top-0 after:-right-8 after:-z-10 after:bg-blue-dark after:border-b-0 after:border-r-0 after:-skew-x-20">
+        <img src="../../assets/images/logos/logo.svg" class="w-full" />
+      </div>
+    </kiosek-container>
+  </header>
+</template>
