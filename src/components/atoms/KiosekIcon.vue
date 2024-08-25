5943b7857fb84807525007d701b9fa40e15bd173
Tereza Mündleinová <tereza.mundleinova@gmail.com>
2024-08-25 10:38:40 +0200

    Icon component created, icons added

---
+++ src/components/atoms/KiosekIcon.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps({
+  iconImage: {
+    type: String,
+    default: null,
+    required: true
+  },
+  iconClass: {
+    type: String,
+    default: null,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div :class="iconClass">
+    <img :src="iconImage" alt="">
+  </div>
+</template>
