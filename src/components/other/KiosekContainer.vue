ec498e734509e638e6a98cd678f156964a20ea92
Tereza Mündleinová <tereza.mundleinova@gmail.com>
2024-08-25 10:40:50 +0200

    Container component created

---
+++ src/components/other/KiosekContainer.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps({
+  backgroundClass: {
+    type: String,
+    required: false,
+    default: 'bg-white',
+  },
+})
+</script>
+
+<template>
+  <div
+    class="max-w-7xl block mx-auto"
+    :class="backgroundClass">
+    <slot />
+  </div>
+</template>
