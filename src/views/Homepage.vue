e031cefd5d5d4c7eb84afcc16f7bc05a93b404e6
Tereza Mündleinová <tereza.mundleinova@gmail.com>
2024-08-25 10:41:06 +0200

    Homepage created and renamed from HomeView

---
+++ src/views/Homepage.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import KiosekButton from '@/components/atoms/KiosekButton.vue'
+import KiosekContainer from '@/components/other/KiosekContainer.vue'
+
+const options = [
+  {
+    buttonText: 'Mám schůzku',
+    buttonPath: 'meetingDetail',
+    icon: 'meeting'
+  },
+  {
+    buttonText: 'Chci koupit vůz',
+    buttonPath: 'home',
+    icon: 'buyCar'
+  },
+  {
+    buttonText: 'Prodám svůj vůz',
+    buttonPath: 'home',
+    icon: 'sellCar'
+  },
+  {
+    buttonText: 'Reklamace',
+    buttonPath: 'home',
+    icon: 'claim'
+  },
+]
+</script>
+
+<template>
+  <main>
+    <kiosek-container>
+      <section class="flex overflow-hidden">
+        <div class="overflow-hidden relative -ml-24 -skew-x-20 max-w-140">
+          <img
+            src="~@/assets/images/layout/car-dealer.png"
+            alt=""
+            class="skew-x-20 origin-top-right">
+        </div>
+        <div class="pl-36 pr-20 py-8 -skew-x-20">
+          <h1 class="text-3xl font-bold text-blue-dark skew-x-20 mb-8">Se kterou službou Vám dnes můžeme pomoci?</h1>
+          <div
+            v-for="(item, index) in options"
+            class="skew-x-20 mb-3">
+            <kiosek-button
+              :index="index"
+              tag="router-link"
+              :to="{ name: item.buttonPath }"
+              primary
+              navigation
+              :icon="item.icon">
+              {{ item.buttonText }}
+            </kiosek-button>
+          </div>
+        </div>
+        <div class="flex flex-col justify-end">
+          <div class="flex items-center pb-2">
+            <p class="text-xl text-blue-dark font-bold mr-2">EN</p>
+            <img
+              src="~@/assets/images/icons/en-flag.png"
+              alt=""
+              class="w-10 h-8">
+          </div>
+        </div>
+
+      </section>
+
+    </kiosek-container>
+  </main>
+</template>
