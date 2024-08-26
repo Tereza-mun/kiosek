45802ba1de61c064a99e876e1db1635ee552e069
Tereza Mündleinová <tereza.mundleinova@gmail.com>
2024-08-26 09:10:12 +0200

    New logo added, previous removed, icon of a person added

--- src/components/organisms/KiosekHeader.vue
+++ src/components/organisms/KiosekHeader.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
 import KiosekContainer from '@/components/other/KiosekContainer.vue'
+import KiosekButton from '@/components/atoms/KiosekButton.vue'
 </script>
 
 <template>
   <header>
     <kiosek-container>
-      <div class="bg-blue-dark pl-6 pr-4 py-3 w-56 relative z-20 after:content-empty after:absolute after:block after:w-12 after:h-full after:top-0 after:-right-8 after:-z-10 after:bg-blue-dark after:border-b-0 after:border-r-0 after:-skew-x-20">
-        <img src="../../assets/images/logos/logo.svg" class="w-full" />
-      </div>
+      <kiosek-button
+        tag="router-link"
+        :to="{name: 'home'}"
+      >
+        <div class="bg-blue-dark pl-6 pr-4 py-3 w-56 relative z-20 after:content-empty after:absolute after:block after:w-12 after:h-full after:top-0 after:-right-8 after:-z-10 after:bg-blue-dark after:border-b-0 after:border-r-0 after:-skew-x-20">
+          <img
+            src="~@/assets/images/logos/nejlepsi-auta.svg"
+            alt="logo nejlepší auta"
+            class="w-full" />
+        </div>
+      </kiosek-button>
     </kiosek-container>
   </header>
 </template>
